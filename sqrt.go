cbf652a5dc17b22013372b6985fccb41f2ed4e30
Krzysztof Kula <krzychukula@gmail.com>
2014-10-31 07:40:01 +0100

    use idea from go-tour to implement diff
    https://tour.golang.org/#25
    BTW. it runs 7 times

--- sqrt.go
+++ sqrt.go
@@ -1,11 +1,32 @@
 //Package newmath is a trivial example package.
 package newmath
 
+import "fmt"
+
+func abs(x float64) float64 {
+	switch {
+	case x < 0:
+		return -x
+	case x == 0:
+		return 0 // return correctly abs(-0)
+	}
+	return x
+}
+
 //Sqrt return an approximation to the square root of x.
 func Sqrt(x float64) float64 {
+	diff := 0.000000000000000001
 	z := 1.0
-	for i := 0; i < 1000; i++ {
-		z -= (z*z - x) / (2 * z)
+	var newZ float64
+	var count = 0
+	for ; ; z = newZ {
+		count += 1
+		newZ = z - (z*z-x)/(2*z)
+		if abs(z-newZ) < diff {
+			break
+		}
+
 	}
+	fmt.Println(count)
 	return z
 }
